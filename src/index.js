e1474cd834ee9370d6b5bdab55e53a456d3c683b
Dmitry.Kononets <d.cononec@gmail.com>
2023-10-10 15:46:32 +0300

    Задача 1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import Zombie from './js/class/zombie';
+import Undead from './js/class/undead';
+import Swordsman from './js/class/swordman';
+import Team from './js/team';
 
-const zombie = new Zombie('Biba');
-console.log(zombie);
+const swordman = new Swordsman('swordman');
+const zombie = new Zombie('zombie');
+const undead = new Undead('undead');
+const team = new Team();
+team.addAll(swordman, zombie, undead);
+console.log(team.iterate());
+console.log(team.iterate());
+console.log(team.iterate());
